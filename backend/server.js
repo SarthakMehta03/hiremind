8ffdf44eec6f5a51f3dc9cddeb368137886b2dde
SarthakMehta03 <sarthakmehta512@gmail.com>
2026-05-23 17:08:43 +0530

    Implemented JWT authentication system

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const PORT = process.env.PORT || 5000;
+const authRoutes = require("./routes/authRoutes");
+const protect = require("./middleware/authMiddleware");
+
 require("dotenv").config();
 
-
-
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("HireMind AI Backend Running");
 });
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
@@ -21,7 +26,14 @@ mongoose.connect(process.env.MONGO_URI)
     console.log(err);
 });
 
+app.get("/api/protected", protect, (req, res) => {
 
+    res.json({
+        message: "Protected route accessed",
+        user: req.user
+    });
+
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
