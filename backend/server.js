30bfdedfebc958a0c91d01d056b5f1878451faed
SarthakMehta03 <sarthakmehta512@gmail.com>
2026-05-23 14:42:19 +0530

    Completed backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
-const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
     console.log(err);
 });
 
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
